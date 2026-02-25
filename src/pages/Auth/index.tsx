7be20a06851b69491b01f8d6cb136b03ac2b0ad0
Chaewon Lee <codenamewont@gmail.com>
2026-02-25 12:22:57 +0900

    feat: login, sigup 폼 컴포넌트 (#4)
    * feat: 라우터 추가
    
    * feat: login, sigup 폼 컴포넌트

---
+++ src/pages/Auth/index.tsx
@@ -0,0 +1,9 @@
+import Auth from './Auth';
+
+export default function AuthPage() {
+  return (
+    <div className="bg-background min-h-[100vh] w-full flex items-center justify-center p-0 m-0">
+      <Auth />
+    </div>
+  );
+}
