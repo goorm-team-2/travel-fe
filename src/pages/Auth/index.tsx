6b00376e38d244c5d3b2763b1b2588d4fef6d9a6
Chaewon Lee <codenamewont@gmail.com>
2026-02-25 15:37:37 +0900

    feat: 로그인/회원가입 ui 완료 (#5)

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -2,7 +2,7 @@ import Auth from './Auth';
 
 export default function AuthPage() {
   return (
-    <div className="bg-background min-h-[100vh] w-full flex items-center justify-center p-0 m-0">
+    <div className="bg-background w-full flex items-center justify-center py-12 m-0">
       <Auth />
     </div>
   );
